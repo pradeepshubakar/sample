404e6d20647c7f10d47d3f17ffa0643be1c49944
Prade <Prade@DESKTOP-UBQJ9QQ>
2020-04-23 14:18:04 +0530

    5th commit

--- NaukriTest/src/test/java/com/naukri/test/UploadResume.java
+++ NaukriTest/src/test/java/com/naukri/test/UploadResume.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.naukri.generics.BaseClass1;
@@ -11,7 +12,7 @@ import com.naukri.generics.FileLib1;
 import com.naukri.generics.webDrivernCommonLib;
 import com.naukri.pom.HomePage1;
 import com.naukri.pom.LoginPage1;
-
+@Listeners(com.naukri.generics.Listeners.class)
 public class UploadResume extends BaseClass1 {
 @Test
 public void testUploadResume() throws IOException, InterruptedException {
